94cc289167b278e6796ba34c03eacf6f3aaa6509
mari-mayo <mariane.maiolini@gmail.com>
2025-10-08 21:41:49 -0300

    feat:Implementado o formulario de Cadastro

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import Login from './pages/Login/Login.jsx'
+import Cadastro from './pages/Cadastro/Cadastro.jsx'
 import "./global.css"
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <Login />
+    {/* <Login /> */}
+    <Cadastro />
   </StrictMode>,
 )
---
+++ src/pages/Cadastro/Cadastro.jsx
@@ -0,0 +1,17 @@
+import "./Cadastro.css"
+
+export default function Cadastro() {
+    return (
+        <div className="cadastro-container">  
+            <form id="cadastro-card" action="">
+                <h2>Cadastro </h2>
+                <input type="text" placeholder="Usuário" />
+                <input type="email" placeholder="Email" />
+                <input type="password" placeholder="Senha..." />
+                <input type="password" placeholder="Confirmar senha..." />
+                <button type="submit">Cadastrar </button>
+            </form>
+
+        </div>
+    )
+}
